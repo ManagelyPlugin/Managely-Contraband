8b895870e8570da093ed7254686305df06ebb363
skrekhere <liam@tannock.net>
2021-07-10 15:40:40 -0700

    yo

--- src/main/java/org/managely/managelycontraband/Main.java
+++ src/main/java/org/managely/managelycontraband/Main.java
@@ -7,12 +7,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.managely.ManagelyCore.ManagelyCore;
 import org.managely.ManagelyCore.modules.ManagelyModule;
 import org.managely.managelycontraband.events.onItemPickup;
-import org.managely.managelycontraband.events.yo;
 
 public class Main extends JavaPlugin implements ManagelyModule {
     @Override
     public void onEnable(){
-        Bukkit.getPluginManager().registerEvents(new yo(), this);
         Bukkit.getPluginManager().registerEvents(new onItemPickup(), this);
         ManagelyCore.registerModule("Contraband", this);
     }
